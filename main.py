9675dd21f98e1f6507dcacfe0dd19a8226d84939
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2022-11-28 20:33:46 -0500

    imported data from csv to mongo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+from data_importer import import_from_csv
+
 from dotenv import load_dotenv
 
 import os
@@ -8,9 +10,8 @@ import os
 load_dotenv()
 
 client = MongoClient("mongodb+srv://" + os.getenv("U_NAME") + ":" + os.getenv("PASS") + "@mlb-predictions.caflwws.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-db = client["test"]
-coll = db["test"]
-val = coll.find_one({'name': 'Yankees'})
+db = client["mlb"]
+coll = db["pitching"]
 
-
-print(val)
+data_pitching = import_from_csv('team_standard_pitching.csv')
+data_batting = import_from_csv('team_standart_batting.csv')
