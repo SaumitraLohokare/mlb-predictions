f10bc64e58b9668c655671349d474b513767ec05
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2022-12-08 15:13:50 -0500

    Predicted playoffs

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ import os
 
 load_dotenv()
 
+# Connecting to MongoDB
 client = MongoClient("mongodb+srv://" + os.getenv("U_NAME") + ":" + os.getenv("PASS") + "@mlb-predictions.caflwws.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 db = client["mlb"]
 pitching_coll = db["pitching"]
 batting_coll = db["batting"]
 
+# Importing data sets
 data_pitching = import_from_csv('team_standard_pitching.csv')
 data_batting = import_from_csv('team_standard_batting.csv')
 
@@ -35,7 +37,9 @@ playoff_teams = (
     "Toronto Blue Jays"
 )
 
+# Task 1
 oRPB = calculate(batting_coll)
+# Task 2
 dRPB = calculate(pitching_coll)
 
 # Filter teams that are in playoffs
@@ -45,34 +49,40 @@ dRPB_playoffs = { k:v for k, v in dRPB.items() if k in playoff_teams }
 # playoff matches combinations
 matches = list(combinations(playoff_teams, 2))
 
-# Simulate matches
-# for match in matches:
-#     print("----------", match[0], " vs", match[1], "----------")
-#     first_half = oRPB_playoffs[match[0]] - dRPB_playoffs[match[1]]
-#     second_half = oRPB_playoffs[match[1]] - dRPB_playoffs[match[0]]
+# Simulate matches --> Task 3
+def predict_winner(teams) -> str:
+    first_half = oRPB_playoffs[teams[0]] - dRPB_playoffs[teams[1]]
+    second_half = oRPB_playoffs[teams[1]] - dRPB_playoffs[teams[0]]
+    if first_half > second_half:
+        return teams[0]
+    else:
+        return teams[1]
 
-#     print(first_half, second_half)
-#     if first_half > second_half:
-#         print("Winner: ", match[0])
-#     else:
-#         print("Winner: ", match[1])
-#     print()
-
-brackets = [
+wildcard_brackets = [
     ("Tampa Bay Rays", "Cleveland Guardians"),
     ("Seattle Mariners", "Toronto Blue Jays"),
     ("Philadelphia Phillies", "St. Louis Cardinals"),
     ("San Diego Padres", "New York Mets")
 ]
 
-for match in brackets:
-    print("----------", match[0], " vs", match[1], "----------")
-    first_half = oRPB_playoffs[match[0]] - dRPB_playoffs[match[1]]
-    second_half = oRPB_playoffs[match[1]] - dRPB_playoffs[match[0]]
+divisional_brackets = [
+    ("New York Yankees", predict_winner(wildcard_brackets[0])),
+    ("Houston Astros", predict_winner(wildcard_brackets[1])),
+    ("Atlanta Braves", predict_winner(wildcard_brackets[2])),
+    ("Los Angeles Dodgers", predict_winner(wildcard_brackets[3])),
+]
 
-    print(first_half, second_half)
-    if first_half > second_half:
-        print("Winner: ", match[0])
-    else:
-        print("Winner: ", match[1])
-    print()
+championship_brackets = [
+    (predict_winner(divisional_brackets[0]), predict_winner(divisional_brackets[1])),
+    (predict_winner(divisional_brackets[2]), predict_winner(divisional_brackets[3])),
+]
+
+overall_winner = predict_winner((predict_winner(championship_brackets[0]), predict_winner(championship_brackets[1])))
+
+pp(wildcard_brackets)
+print()
+pp(divisional_brackets)
+print()
+pp(championship_brackets)
+print()
+print(overall_winner)
